3672d5b548fc1d7eeab1b1f29c77f082f6a5adea
Maikel <maikelmysilvestre@gmail.com>
2022-04-13 18:58:15 -0300

    dia 5.1 finalizado

---
+++ 05-Bloco:JavaScript:DOM,-Eventos-e-Web-Storage/dia_01/script.js
@@ -0,0 +1,38 @@
+// document.getElementById('header-container').style.backgroundColor = "green";
+document.querySelector("#header-container").style.backgroundColor = "green";
+
+// let a = document.getElementsByClassName('emergency-tasks');
+// for(index in a){
+//     a[index].style.color = 'black';
+// }
+
+// em = document.querySelectorAll("emergency-tasks");
+// for(index in em){
+//     em[index].style.color = "black";
+// }
+document.querySelectorAll(".emergency-tasks")[0].style.backgroundColor = "gray";
+
+
+document.querySelectorAll(".emergency-tasks h3")[0].style.backgroundColor = "purple";
+document.querySelectorAll(".emergency-tasks h3")[1].style.backgroundColor = "purple";
+
+
+document.querySelectorAll(".no-emergency-tasks")[0].style.backgroundColor = "yellow";
+document.querySelectorAll(".no-emergency-tasks h3")[0].style.backgroundColor = "black";
+document.querySelectorAll(".no-emergency-tasks h3")[1].style.backgroundColor = "black";
+
+
+// const abc = document.querySelectorAll(".no-emergency-tasks h3");
+// for(index in abc){
+//     abc[index].style.backgroundColor = "black";
+// }
+// const emergencia = document.querySelectorAll(".emergency-tasks h3");
+// for(index in emergencia){
+//     emergencia[index].style.backgroundColor = "purple";
+// }
+
+
+// const nEmergencia = document.querySelectorAll(".no-emergency-tasks h3");
+// for(index in nEmergencia){
+//     nEmergencia[index].style.backgroundColor = "black";
+// }

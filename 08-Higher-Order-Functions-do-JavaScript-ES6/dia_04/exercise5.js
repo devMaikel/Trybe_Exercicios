d13df50aea383be221de025805bda7c0821d8c06
Maikel <maikelmysilvestre@gmail.com>
2022-05-19 15:47:37 -0300

    atualizando dia 8.4

--- 08-Higher-Order-Functions-do-JavaScript-ES6/dia_04/exercise5.js
+++ 08-Higher-Order-Functions-do-JavaScript-ES6/dia_04/exercise5.js
@@ -5,11 +5,9 @@ const names = [
 ];
  //Dada o array de nomes, retorne a quantidade de vezes em que aparecem a letra a maiúscula ou minúscula.
 function containsA() {
-  const qtd = names.filter((vezesTotal, nomeAtual) => {
-    for(i = 0; i < nomeAtual.length; i += 1){
-      console.log('haha');
-    }
-  }, 0)
+  const qtd = names.reduce((vezesTotal, nomeAtual) => {
+    console.log(nomeAtual)
+  })
 
   console.log(qtd);
 }

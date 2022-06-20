5fcb2dca681a66396df6494411bf69fb8daea476
Maikel <maikelmysilvestre@gmail.com>
2022-06-20 16:03:36 -0300

    10.1 finalizado

---
+++ 10-Introducao-a-React/dia_01/test/testando-meu-computador/src/components/HelloWorld.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+class HelloWorld extends React.Component {
+  render() {
+    return (
+    <div className="helloworld">
+      <h1>
+        Oiii mundo !!!
+      </h1>
+    </div>
+    )
+  }
+}
+
+export default HelloWorld;
